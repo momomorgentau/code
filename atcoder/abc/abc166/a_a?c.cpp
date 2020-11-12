c58175fdbbf9884ed6b4fc26fcce9ef1d6dc76d9
sslmatsui <sslmatsui0214@gmail.com>
2020-11-13 01:42:43 +0900

    abc166

---
+++ atcoder/abc/abc166/a_a?c.cpp
@@ -0,0 +1,13 @@
+//0
+//abc166_a_a?c
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+  string ans = s == "ABC"?"ARC":"ABC";
+  cout << ans << endl; 
+  return 0;
+}
--- atcoder/abc/abc168/a_therefore.cpp
+++ atcoder/abc/abc168/a_therefore.cpp
@@ -1,2 +1,23 @@
 //2
 //abc168_a_therefore
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i =0;i<n;++i)
+using namespace std;
+using ll = long long;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int d = n % 10;
+	string ans;
+	if (d == 2 || d == 4 || d == 5 || d == 7 || d == 9) 
+	{
+		ans = "hon";
+	}
+	else if (d == 3) ans = "bon";
+	else ans = "pon";
+	cout << ans << endl;
+
+	return 0;
+}
