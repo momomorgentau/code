705ea06594d587e217112c21c533cc65f83a6b3b
momomorgentau <sslmatsui0214@gmail.com>
2020-11-16 09:17:26 +0900

    abc183de

--- atcoder/abc/abc183/d_water_heater.cpp
+++ atcoder/abc/abc183/d_water_heater.cpp
@@ -1,2 +1,76 @@
 //diff617
 //abc183_d_water_heater
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define reps(i,s,n) for(int i=s;i<n;++i)
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+
+//シミュレーション
+int solve1()
+{
+
+    int n, w;
+    cin >> n >> w;
+    vector<P> e;
+    rep(i, n)
+    {
+        int s, t, p;
+        cin >> s >> t >> p;
+        e.emplace_back(s, p);
+        e.emplace_back(t, -p);
+    }
+
+    //まず、時間でソート、時間が同じ場合は負の値が先に来る
+    sort(e.begin(), e.end());
+    ll need = 0;
+    for (P ei : e)
+    {
+        int pi;
+        tie(ignore, pi) = ei;
+        need += pi;
+        if (need > w)
+        {
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+
+    return 0;
+}
+//いもす法
+int solve2()
+{
+    const int t_max = 2 * 1e5;
+    int n, w;
+    cin >> n >> w;
+
+    vector<ll> a(t_max + 1, 0);
+
+    rep(i, n)
+    {
+        int s, t, p;
+        cin >> s >> t >> p;
+        a[s] += p;
+        a[t] -= p;
+    }
+
+    rep(i, t_max) a[i + 1] += a[i];
+
+    ll max_w = *max_element(a.begin(), a.end());
+
+    if (max_w <= w) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
+
+int main()
+{
+    //solve1();
+    solve2();
+    return 0;
+}
+
--- atcoder/abc/abc183/e_queen_on_grid.cpp
+++ atcoder/abc/abc183/e_queen_on_grid.cpp
@@ -1,2 +1,69 @@
 //diff1246
 //abc183_e_queen_on_grid
+
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define reps(i,s,n) for(int i=s;i<n;++i)
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+
+const int mod = 1e9 + 7;
+
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+    vector<vector<bool>> g(h, vector<bool>(w));
+    rep(i, h)rep(j, w)
+    {
+        char c; cin >> c;
+        if (c == '#') g[i][j] = false;
+        else g[i][j] = true;
+    }
+    vector<vector<ll>> dp(h, vector<ll>(w));
+    vector<vector<ll>> row(h, vector<ll>(w));
+    vector<vector<ll>> col(h, vector<ll>(w));
+    vector<vector<ll>> dia(h, vector<ll>(w));
+
+    dp[0][0] = 1;
+    row[0][0] = 0;
+    col[0][0] = 0;
+    dia[0][0] = 0;
+
+    rep(i, h)rep(j, w)
+    {
+        int x = j;
+        int y = i;
+        //‰¡•ûŒü
+        int rx = x + 1;
+        if (rx < w && g[y][rx])
+        {
+            row[y][rx] += row[y][x] + dp[y][x];
+            row[y][rx] %= mod;
+            dp[y][rx] += row[y][rx];
+            dp[y][rx] %= mod;
+        }
+        //c•ûŒü
+        int dy = y + 1;
+        if (dy < h && g[dy][x])
+        {
+            col[dy][x] += col[y][x] + dp[y][x];
+            col[dy][x] %= mod;
+            dp[dy][x] += col[dy][x];
+            dp[dy][x] %= mod;
+        }
+        //ŽÎ‚ß•ûŒü
+        if (rx < w && dy < h && g[dy][rx])
+        {
+            dia[dy][rx] += dia[y][x] + dp[y][x];
+            dia[dy][rx] %= mod;
+            dp[dy][rx] += dia[dy][rx];
+            dp[dy][rx] %= mod;
+        }
+    }
+
+    cout << dp[h - 1][w - 1] << endl;
+    return 0;
+}
+
