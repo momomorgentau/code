1206b8bdc423f5e88a164ae08164f5f600e2ba68
momomorgentau <sslmatsui0214@gmail.com>
2020-12-16 19:22:28 +0900

    segtree

--- library/segment_tree.cpp
+++ library/segment_tree.cpp
@@ -1,29 +1,56 @@
-template<typename T>
-struct segtree {
-    const T inf = numeric_limits<T>::max();
+//0-indexed
+template<class T, T(*op)(T, T), T(*e)()>
+class segtree
+{
+private:
     int n;
     vector<T> v;
-    segtree(int n_) { //ˆø”‚Ìn‚¾‚©‚çn_,struct“à‚Ìn‚Æ‹æ•Ê‚·‚éB
+public:
+    segtree(int n_)
+    {
         int x = 1;
-        while (n_ > x) x *= 2;
+        while (n_ > x) x <<= 1;
         n = x;
-        v.resize(2 * n - 1, inf);
+        v.resize(2 * n - 1, e());
     }
-
-    void update(int k, T x) {
+    void set(int k, T x)
+    {
         k += n - 1;
         v[k] = x;
-        while (k > 0) {
+        while (k > 0)
+        {
             k = (k - 1) / 2;
-            v[k] = min(v[k * 2 + 1], v[k * 2 + 2]);
+            v[k] = op(v[k * 2 + 1], v[k * 2 + 2]);
         }
     }
+    T get(int k) { return v[k + n - 1]; }
+    //[a,b)
     T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
     T query_sub(int a, int b, int k, int l, int r) {
-        if (b <= l || r <= a) return inf;
+        if (b <= l || r <= a) return e();
         if (a <= l && r <= b) return v[k];
         T c1 = query_sub(a, b, 2 * k + 1, l, (l + r) / 2);
         T c2 = query_sub(a, b, 2 * k + 2, (l + r) / 2, r);
-        return min(c1, c2);
+        return op(c1, c2);
+    }
+
+    //display segtree value
+    void display(string text = "segtree value")
+    {
+        cout << text << endl;
+        int cnt = 0;
+        int end = 1 << cnt;
+        int l = v.size();
+        for (int i = 0; i < l; ++i)
+        {
+            cout << v[i] << " ";
+            if (i == end - 1)
+            {
+                ++cnt;
+                end += 1 << cnt;
+                cout << endl;
+            }
+        }
+        cout << endl;
     }
 };
