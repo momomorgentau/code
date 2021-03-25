2c7aaea7f27cd1d4515a5a708e6178121f386405
sslmatsui <sslmatsui0214@gmail.com>
2021-03-25 18:47:08 +0900

    abc117

---
+++ atcoder/abc/abc117/c_streamline.cpp
@@ -0,0 +1,14 @@
+//ABC_117_C_Streamline
+//612
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define reps(i,s,n) for(int i=s;i<n;++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+int main()
+{
+
+
+    return 0;
+}
