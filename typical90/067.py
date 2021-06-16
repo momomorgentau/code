aa376aa935073d7114a28c0c9dd0fb13f0394a20
momomorgentau <sslmatsui0214@gmail.com>
2021-06-16 09:09:26 +0900

    067（TLE）の追加

---
+++ typical90/067.py
@@ -0,0 +1,19 @@
+//066
+//*2n,k = map(int, input().split())
+if n == 0:
+  print(0)
+  exit()
+
+s = str(n)  
+for _ in range(k):
+  now = 0
+  for c in s:
+    now *= 8
+    now += int(c)
+  res = ""
+  while now > 0:
+    res += str(now % 9)
+    now //= 9
+  s = res[::-1].replace("8","5")
+
+print(s)
