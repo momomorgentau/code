015aa6bea410bdc7338048739e835ecf3c470c35
momomo <sslmatsui0214@gmail.com>
2022-01-22 09:06:29 +0900

    abc164d

--- atcoder/abc/abc164/d_multiple_of_2019.cpp
+++ atcoder/abc/abc164/d_multiple_of_2019.cpp
@@ -1,16 +1,44 @@
+//----------------------------
+//ABC_164_D
 //1232
-//164_d_multiple_of_2019
+//Multiple 2019
+//----------------------------
 #include<bits/stdc++.h>
-#define rep(i,n) for(int i =0;i<n;++i)
+#define rep(i,n) for(int i=0;i<n;++i)
+#define reps(i,s,n) for(int i=s;i<n;++i)
 using namespace std;
-using P = pair<int,int>;
+using ll = long long;
+using P = pair<int, int>;
+using T = tuple<int, int, int>;
+
 
 int main()
 {
-    rep(i,50)
-    {
-        cout << 2019*i << endl;
-    }
+	const int MOD = 2019;
+	string s;
+	cin >> s;
+	reverse(s.begin(), s.end());
+	int sz = s.size();
+	vector<int> a(sz);
+	vector<int> S(sz + 1);
+	int t = 1;
+	rep(i, sz)
+	{
+		a[sz - i - 1] = (s[i] - '0') * t % MOD;
+		t = (t * 10) % MOD;
+	}
+	rep(i, sz)
+	{
+		S[i + 1] = (S[i] + a[sz - 1 - i]) % MOD;
+	}
+	map<int, int> mp;
+	ll ans = 0;
+	rep(i, sz + 1)
+	{
+		ans += mp[S[i]];
+		++mp[S[i]];
+	}
+	cout << ans << endl;
 
-    return 0;
+	return 0;
 }
