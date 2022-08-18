de876f11b8d8679b94b3e662cce73783039604df
momomo <sslmatsui0214@gmail.com>
2022-08-19 05:40:08 +0900

    h

---
+++ others/edpc/h_grid1.cpp
@@ -0,0 +1,94 @@
+//Educational DP Contest H
+//Grid1
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define reps(i,s,n) for(int i=s;i<n;++i)
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+
+const int mod = 1e9 + 7;
+class mint
+{
+public:
+    long long x;
+    mint(long long x = 0) :x((x% mod + mod) % mod) {}
+    mint operator -() const { return mint(-x); }
+    mint& operator +=(const mint a)
+    {
+        if ((x += a.x) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator -=(const mint a)
+    {
+        if ((x += -a.x + mod) >= mod) x -= mod;
+        return *this;
+    }
+    mint& operator *=(const mint a)
+    {
+        (x *= a.x) %= mod;
+        return *this;
+    }
+    mint operator+(const mint a)
+    {
+        mint res(*this);
+        return res += a;
+    }
+    mint operator-(const mint a)
+    {
+        mint res(*this);
+        return res -= a;
+    }
+    mint operator*(const mint a)
+    {
+        mint res(*this);
+        return res *= a;
+    }
+    mint pow(long long t) const
+    {
+        if (!t) return 1;
+        mint a = pow(t >> 1);
+        a *= a;
+        if (t & 1) a *= *this;
+        return a;
+    }
+    //for only prime number
+    //Fermat's little theorem
+    mint inv() const
+    {
+        return pow(mod - 2);
+    }
+    mint& operator/=(const mint a)
+    {
+        return (*this) *= a.inv();
+    }
+    mint operator/(const mint a)
+    {
+        mint res(*this);
+        return res /= a;
+    }
+};
+
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+    vector<string> g(h);
+    rep(i, h) cin >> g[i];
+    vector<vector<mint>> dp(h, vector<mint>(w, 0));
+    dp[0][0] = 1;
+    rep(i, h)rep(j, w)
+    {
+        if (i + 1 < h && g[i + 1][j] == '.')
+        {
+            dp[i + 1][j] += dp[i][j];
+        }
+        if (j + 1 < w && g[i][j + 1] == '.')
+        {
+            dp[i][j + 1] += dp[i][j];
+        }
+    }
+    cout << dp[h - 1][w - 1].x << endl;
+
+    return 0;
+}
